f9c02d350d6997f1f54fda61abec9b410a549082
Patrick <patrickpqdt87289@gmail.com>
2022-05-08 08:44:05 -0300

    settings-to-deploy

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 import { routes } from './routes'
 
     const app = express()
-    const port = 5000
+    const port = 3333
     app.use(cors());
     // app.use(cors({
     //     origin: 'http://localhost:3000'
@@ -11,4 +11,4 @@ import { routes } from './routes'
     app.use(express.json())
     app.use(routes)
  
-app.listen(port,() => console.log(`Server running on the port ${port}`))
+app.listen(port,() => console.log(`HTTP server running`))
